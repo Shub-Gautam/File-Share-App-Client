53f1d9f4a22c1146eec629cbd11dd0717c1adac1
Shubham Gautam <72097846+Shub-Gautam@users.noreply.github.com>
2021-08-06 10:12:28 +0530

    port fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log("Listening on port 8000");
 });
 
